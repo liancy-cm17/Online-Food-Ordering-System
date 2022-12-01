d248f6a23b3eb62e5972583c511839bc86b4cc0e
LianCY <79098917+liancy-cm17@users.noreply.github.com>
2022-12-01 11:47:26 +0800

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> All rights reserved. Developed By Online Food Ordering System</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
